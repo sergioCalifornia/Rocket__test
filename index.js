748d022ea1111a14cd16c57314c3936d2c79ab79
sergioCalifornia <Li_zard161@mail.ru>
2021-12-07 06:10:41 +0300

    test

---
+++ index.js
@@ -0,0 +1,56 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination'	
+    ,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+});
+
+
+// Отправка заявки 
+$(document).ready(function() {
+	$('#form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
+		if (document.form.name.value == '' || document.form.phone.value == '' || document.form.email.value == '')  {
+			valid = false;
+			return valid;
+		}
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$('.js-overlay-thank-you').fadeIn();
+			$(this).find('input').val('');
+			$('#form').trigger('reset');
+		});
+		return false;
+	});
+});
+
+// Закрыть попап «спасибо»
+$('.js-close-thank-you').click(function() { // по клику на крестик
+	$('.js-overlay-thank-you').fadeOut();
+});
+
+$(document).mouseup(function (e) { // по клику вне попапа
+    var popup = $('.popup');
+    if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+        $('.js-overlay-thank-you').fadeOut();
+    }
+});
+
+// Маска ввода номера телефона (плагин maskedinput)
+$(function($){
+	$('[name="phone"]').mask("+7(999) 999-9999");
+});
---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php
+
+$recepient = "Li_zard161@mail.ru";
+$siteName = "Rocket-test";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$email = trim($_POST["email"]);
+$message = "Имя: $name \nТелефон: $phone \nEmail: $email";
+
+$pagetitle = "Заявка с сайта \"$siteName\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
